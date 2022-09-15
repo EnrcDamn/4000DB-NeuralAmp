6972180ea793e7f32e6bf0f763deac4441fc72f6
EnrcDamn <enrico.damiani10@gmail.com>
2022-09-15 19:31:25 +0200

    Implemented save_wav function

--- Model/data_processing.py
+++ Model/data_processing.py
@@ -4,21 +4,28 @@ import os
 from scipy.io import wavfile
 import numpy as np
 
+IN_FILE = "4000DB-dry"
+OUT_FILE = "4000DB-100"
+
 class Dataset:
-    def __init__(self, in_filename, out_filename='', data_dir='Model/Data/'):
+    def __init__(self, in_filename, out_filename, data_dir='Model/Data/'):
         self.data_dir = data_dir
         self.in_filename = in_filename
         self.out_filename = out_filename
 
     def process(self):
-        in_rate, in_data = self.load_file(os.path.join(self.data_dir, self.in_filename))
-        out_rate, out_data = self.load_file(os.path.join(self.data_dir, self.out_filename))
+        in_rate, in_data = self.load_file(os.path.join(self.data_dir, self.in_filename+'.wav'))
+        out_rate, out_data = self.load_file(os.path.join(self.data_dir, self.out_filename+'.wav'))
         assert in_rate == out_rate, "in_data and out_data must have same sample rate!"
-        in_data = self.audio_converter(in_data)
-        out_data = self.audio_converter(out_data)
+        #in_data = self.audio_converter(in_data)
+        #out_data = self.audio_converter(out_data)
         # in_data = normalize(in_data)
         x_subset = self.audio_splitter(in_data, 'x')
         y_subset = self.audio_splitter(out_data, 'y')
+        self.save_wav('Model/processed/'+self.in_filename+'_train.wav', in_rate, x_subset['x_train'])
+        self.save_wav('Model/processed/'+self.in_filename+'_test.wav', in_rate, x_subset['x_test'])
+        self.save_wav('Model/processed/'+self.out_filename+'_train.wav', out_rate, y_subset['y_train'])
+        self.save_wav('Model/processed/'+self.out_filename+'_test.wav', out_rate, y_subset['y_test'])
         return x_subset, y_subset
 
     def load_file(self, filename):
@@ -58,11 +65,18 @@ class Dataset:
         Lambda function will put 70% of audio in the first split (train set)
         and 85% in the second one (validation set).
         '''
-        split = lambda d: np.split(d, [int(len(d) * 0.7), int(len(d) * 0.85)])
+        split = lambda d: np.split(d, [int(len(d) * 0.8), int(len(d) * 0.85)])
         slices = {}
         slices[set_name+"_train"], slices[set_name+"_valid"], slices[set_name+"_test"] = split(audio)
         slices["mean"], slices["std"] = slices[set_name+"_train"].mean(), slices[set_name+"_train"].std()
         # standardize
-        for key in set_name+"_train", set_name+"_valid", set_name+"_test":
-            slices[key] = (slices[key] - slices["mean"]) / slices["std"]
+        # for key in set_name+"_train", set_name+"_valid", set_name+"_test":
+        #     slices[key] = (slices[key] - slices["mean"]) / slices["std"]
         return slices
+    
+    def save_wav(self, name, rate, data):
+        wavfile.write(name, rate, data.astype(np.float32))
+
+if __name__ == "__main__":
+    data = Dataset(in_filename=IN_FILE, out_filename=OUT_FILE)
+    x, y = data.process()
